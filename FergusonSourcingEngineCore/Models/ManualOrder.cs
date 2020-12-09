10f41d0c4a8762fdf5ce5b50a7b7be88597a93b4
Hank Warner <40283259+hankwarner@users.noreply.github.com>
2020-12-09 14:29:00 -0500

    Refactored CreateManualOrder to be a standalone cosmosDB trigger.

--- FergusonSourcingEngineCore/Models/ManualOrder.cs
+++ FergusonSourcingEngineCore/Models/ManualOrder.cs
@@ -1,10 +1,86 @@
 ﻿using System.Collections.Generic;
-using Newtonsoft.Json;
+using System.Linq;
 
 namespace FergusonSourcingCore.Models
 {
     public partial class ManualOrder
     {
+        public ManualOrder(AtgOrderRes order, TrilogieRequest trilogieReq = null)
+        {
+            id = order.id;
+            atgOrderId = order.atgOrderId;
+            custAccountId = order.custAccountId;
+            customerId = order.customerId;
+            customerName = order.customerName;
+            orderSubmitDate = order.orderSubmitDate;
+            sellWhse = order.sellWhse ?? "D98 DISTRIBUTION CENTERS";
+            sourcingMessage = order.sourcingMessage;
+            sourceSystem = order.sourceSystem;
+            orderRequiredDate = order.orderRequiredDate;
+            trilogieErrorMessage = trilogieReq?.TrilogieErrorMessage ?? "";
+            trilogieOrderId = trilogieReq?.TrilogieOrderId ?? "";
+            trilogieStatus = trilogieReq?.TrilogieStatus.ToString() ?? "Waiting on Trilogie response.";
+
+            paymentOnAccount = new ManualPaymentOnAccount()
+            {
+                payment = new ManualPayment()
+                {
+                    address1 = order.paymentOnAccount?.First().address1,
+                    address2 = order.paymentOnAccount?.First().address2,
+                    cardType = order.paymentOnAccount?.First().cardType,
+                    city = order.paymentOnAccount?.First().city,
+                    state = order.paymentOnAccount?.First().state,
+                    zip = order.paymentOnAccount?.First().zip,
+                    phone = order.paymentOnAccount?.First().phone
+                }
+            };
+
+            shipping = new ManualShipping()
+            {
+                price = order.shipping?.price,
+                shipViaCode = order.shipping?.shipViaCode,
+                shipVia = order.shipping?.shipVia,
+                shipTo = new ManualShipTo()
+                {
+                    address1 = order.shipping.shipTo?.address1,
+                    address2 = order.shipping.shipTo?.address2,
+                    city = order.shipping.shipTo?.city,
+                    country = order.shipping.shipTo?.country,
+                    name = order.shipping.shipTo?.name,
+                    shipInstructionsPhoneNumberAreaDialing = order.shipping.shipTo?.shipInstructionsPhoneNumberAreaDialing,
+                    shipInstructionsPhoneNumberDialNumber = order.shipping.shipTo?.shipInstructionsPhoneNumberDialNumber,
+                    state = order.shipping.shipTo?.state,
+                    zip = order.shipping.shipTo?.zip
+                }
+            };
+
+            sourcing = order.items.GroupBy(item =>
+                item.shipFrom,
+                item => item,
+                (key, group) => new Sourcing()
+                {
+                    shipFrom = key,
+                    sourceComplete = order.sourceComplete,
+                    items = group.Select(item => new ManualItem()
+                    {
+                        lineItemId = item.lineId,
+                        unitPrice = item.unitPrice,
+                        unitPriceCode = item.unitPriceCode,
+                        extendedPrice = item.extendedPrice,
+                        description = item.itemDescription,
+                        quantity = item.quantity,
+                        sourcingMessage = item.sourcingMessage,
+                        masterProdId = item.masterProdId,
+                        sourcingGuide = item.sourcingGuide,
+                        vendor = item.vendor,
+                        preferredShipVia = item.preferredShipVia,
+                        alt1Code = item.alt1Code
+                    }).ToList()
+                }).ToList();
+        }
+
+        public ManualOrder(){ }
+
         public string atgOrderId { get; set; }
         public string custAccountId { get; set; }
         public string customerId { get; set; }
@@ -14,10 +90,10 @@ namespace FergusonSourcingCore.Models
         public string sellWhse { get; set; }
         public string sellLogon { get; set; }
         public string sourcingMessage { get; set; }
-        public string processOrderType { get; set; }
-        public bool claimed { get; set; }
+        public string processOrderType { get; set; } = "manual";
+        public bool claimed { get; set; } = false;
         public string timeClaimed { get; set; } = null;
-        public bool orderComplete { get; set; }
+        public bool orderComplete { get; set; } = false;
         public string timeCompleted { get; set; } = null;
         public ManualPaymentOnAccount paymentOnAccount { get; set; }
         public ManualShipping shipping { get; set; }
@@ -85,7 +161,7 @@ namespace FergusonSourcingCore.Models
         public string quantity { get; set; }
         public string sourcingMessage { get; set; }
         public string masterProdId { get; set; }
-        public bool itemComplete { get; set; }
+        public bool itemComplete { get; set; } = false;
         public string sourcingGuide { get; set; }
         public string vendor { get; set; }
         public string preferredShipVia { get; set; }
