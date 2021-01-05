e741d05163396087907d3d11fd762783d631d8f4
hankwarner <warnerh1576@gmail.com>
2021-01-05 11:15:24 -0500

    Moving estimated ship cost to new ups-microservices in Release.

--- FergusonSourcingEngineCore/Models/ShipQuote.cs
+++ FergusonSourcingEngineCore/Models/ShipQuote.cs
@@ -4,35 +4,6 @@ using System.Text;
 
 namespace FergusonSourcingCore.Models
 {
-#if RELEASE
-    public class ShipQuoteRequest
-    {
-        public Package package { get; set; }
-    }
-    public class Package
-    {
-        public double length { get; set; }
-        public double width { get; set; }
-        public double height { get; set; }
-        public double weight { get; set; }
-        public string address1 { get; set; }
-        public string address2 { get; set; }
-        public string city { get; set; }
-        public string state { get; set; }
-        public string zip { get; set; }
-        public string originaddress1 { get; set; }
-        public string originaddress2 { get; set; }
-        public string origincity { get; set; }
-        public string originstate { get; set; }
-        public string originzip { get; set; }
-    }
-
-    public class ShipQuoteResponse
-    {
-        public Dictionary<string, double> rates { get; set; }
-    }
-#endif
-#if DEBUG
     public class ShipQuoteRequest
     {
         public string RateType { get; set; }
@@ -49,5 +20,4 @@ namespace FergusonSourcingCore.Models
     {
         public double Weight { get; set; }
     }
-#endif
 }
